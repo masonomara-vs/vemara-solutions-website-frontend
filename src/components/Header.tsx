89a0a33039d231a26b209aaa2d79e68e8615df62
masonomara <mason.omara@gmail.com>
2024-07-16 09:11:34 -0400

    wrapped the header for responsiveness

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,12 +4,15 @@ import styles from "../styles/Header.module.css"
 export default function Header({ label, title, subtitle }: { label: string, title: string, subtitle?: string }) {
 
   return (
-    <div className={styles.container}>
-      <h1 className={styles.label}>{label}</h1>
-      <div className={styles.contentContainer}>
-        <p className={styles.title}>{title}</p>
-        <p className={styles.subtitle}>{subtitle}</p>
+    <div className={styles.wrapper}>
+      <div className={styles.container}>
+        <h1 className={styles.label}>{label}</h1>
+        <div className={styles.contentContainer}>
+          <p className={styles.title}>{title}</p>
+          <p className={styles.subtitle}>{subtitle}</p>
+        </div>
       </div>
     </div>
+
   )
 }
