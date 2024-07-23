bd8a79203253eb416dd02b285ff3d45a2a1ca961
masonomara <mason.omara@gmail.com>
2024-07-23 08:35:56 -0400

    introduced motion to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import styles from "../styles/Header.module.css"
+import { motion } from "framer-motion"
+import { fadeIn, slideIn, staggerContainer } from '../pages/utils/motion'
 
 export default function Header({ label, title, subtitle }: { label: string, title: string, subtitle?: string }) {
 
   return (
     <div className={styles.wrapper}>
       <div className={styles.container}>
-        <h1 className={`location ${styles.label}`}>{label}</h1>
+        <motion.h1 className={`location ${styles.label}`} variants={slideIn("up", "spring", 0, 0.8)}
+          initial="hidden"
+          whileInView="show"
+          viewport={{ once: true, amount: 0 }}>{label}</motion.h1>
         <div className={styles.contentContainer}>
           <p className={`title ${styles.title}`}>{title}</p>
           <p className={`subtitle ${styles.subtitle}`}>{subtitle}</p>
