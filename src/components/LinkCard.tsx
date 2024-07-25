632d0069ff1407d4673e8b0880a6ddf88966be13
masonomara <mason.omara@gmail.com>
2024-07-25 13:51:41 -0400

    finished contact page

--- src/components/LinkCard.tsx
+++ src/components/LinkCard.tsx
@@ -2,6 +2,7 @@ import styles from "../styles/LinkCard.module.css";
 import Link from "next/link";
 import { motion } from "framer-motion";
 import { fadeIn } from "../../utils/motion";
+import Image from "next/image";
 
 export default function LinkCard({
   title,
@@ -9,7 +10,8 @@ export default function LinkCard({
   buttonTitle,
   buttonLink,
   button2Title,
-  button2Link
+  button2Link,
+  delay = 0
 }: {
   title: string;
   description: string;
@@ -17,31 +19,29 @@ export default function LinkCard({
   buttonLink: string;
   button2Title?: string;
   button2Link?: string;
+  delay?: number
 }) {
   return (
     <div className={styles.linkCardWrapper}>
       <motion.div
         key={title}
-        variants={fadeIn("up", "spring", 0, 0.8)}
+        variants={fadeIn("up", "spring", { delay }, 0.8)}
         initial="hidden"
         whileInView="show"
         viewport={{ once: true, amount: 0 }} // Adjust amount as needed
       >
         <div className={styles.linkContainer}>
-          <h2>{title}</h2>
-          <div className={styles.linkDescription}>{description}</div>
-          <div className={styles.linkAction}>
-            <Link href={buttonLink} target={"_top"}>
-              {buttonTitle} -&gt;
-            </Link>
-          </div>
-          {button2Title ? 
-          <div className={styles.link2Action}>
-          <Link href={button2Link ?? ''} target={"_top"}>
-            {button2Title} -&gt;
+          <h4 className="label">{title}</h4>
+          <div className="description">{description}</div>
+
+          <Link className="buttonPrimaryBackground" target="_top" href={buttonLink} >
+            <span className={`callout`}>{buttonTitle}</span><Image height={12.87} width={12.87} src="/clientActionArrowWhite.svg" priority alt=""></Image>
           </Link>
-        </div>
-        : null}
+          {button2Title ?
+            <Link className="buttonSecondaryBackground" target="_top" href={button2Link ?? ''} >
+              <span className={`callout`}>{button2Title}</span><Image height={12.87} width={12.87} src="/clientActionArrowWhite.svg" priority alt=""></Image>
+            </Link>
+            : null}
         </div>
       </motion.div>
     </div>
