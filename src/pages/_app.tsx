204988fb3cc434189e5c6546ba586032d60e69fe
masonomara <mason.omara@gmail.com>
2024-10-29 07:24:34 -0600

    added google analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,6 +5,7 @@ import { AnimatePresence } from 'framer-motion';
 import { Analytics } from '@vercel/analytics/react';
 import { Inter } from "next/font/google";
 import Footer from "@/components/Footer";
+import GoogleAnalytics from "@/components/GoogleAnalytics";
 
 
 const inter = Inter({ subsets: ["latin"] });
@@ -13,10 +14,13 @@ const inter = Inter({ subsets: ["latin"] });
 export default function App({ Component, pageProps, router }: AppProps) {
   return (
     <div className={inter.className}>
+      <GoogleAnalytics />
+
       <AnimatePresence >
         <Component key={router.route} {...pageProps} />
         <Analytics />
+
       </AnimatePresence>
-      <Footer/>
+      <Footer />
     </div>);
 }
